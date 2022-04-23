b540697b155bf59c93d16d54d36d332650ea7b38
Kishn0109 <raikaran159@gmail.com>
2022-04-23 07:22:36 +0530

    created banner.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react"
 
-export default class Navbar extends Component{
-    render(){
+export default class Navbar extends Component {
+    render() {
         return (
             <>
-                <div className="Navbar" style={{ display: "flex", padding: "0.5rem"}}>
-                    <h1>Movie App</h1>
-                    <h2 style={{ marginTop: "2rem", marginLeft: "1.8rem"}}>Favourites</h2>
+                <div className="Navbar" style={{ display: "flex", padding: "0.5rem" }}>
+                    <h1>Movie App hellow how are you </h1>
+                    <h2 style={{ marginTop: "2rem", marginLeft: "1.8rem" }}>Favourites</h2>
                 </div>
             </>
         )
